e2bdcfe6e21a36e62ec1ea97b4b1220d81afa5d0
MINTCOACHING <74488802+MINTCOACHING@users.noreply.github.com>
2020-11-19 09:14:55 +0100

    Add files via upload

---
+++ top10.js
@@ -0,0 +1,86 @@
+
+
+window.onload = clean;
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  var dragSrcEl = null;
+  
+  function handleDragStart(e) {
+    this.style.opacity = '0.4';
+    
+    dragSrcEl = this;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
+
+  function handleDragOver(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+
+    e.dataTransfer.dropEffect = 'move';
+    
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove('over');
+  }
+
+  function handleDrop(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation(); // stops the browser from redirecting.
+    }
+    
+    if (dragSrcEl != this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+    
+    return false;
+  }
+
+  function handleDragEnd(e) {
+    this.style.opacity = '1';
+    
+    items.forEach(function (item) {
+      item.classList.remove('over');
+    });
+  }
+  
+  
+  let items = document.querySelectorAll('.container .box');
+  items.forEach(function(item) {
+    item.addEventListener('dragstart', handleDragStart, false);
+    item.addEventListener('dragenter', handleDragEnter, false);
+    item.addEventListener('dragover', handleDragOver, false);
+    item.addEventListener('dragleave', handleDragLeave, false);
+    item.addEventListener('drop', handleDrop, false);
+    item.addEventListener('dragend', handleDragEnd, false);
+  });
+});
+
+function clean(){
+	var str = '';
+	var zaehler = 0;
+	var counter3 = localStorage.getItem('counter2');
+	for ( i = 1; i<=counter3;i++){
+		document.getElementById(i).innerHTML = 'MAEH';
+		zaehler += 1;
+		str = 'wertetopten' + zaehler;
+		if(localStorage.getItem(str)!==null){
+		document.getElementById(i).innerHTML = 'MUH';
+		document.getElementById(i).innerHTML = localStorage.getItem(str);
+		}else {
+			str = zaehler;
+			document.getElementById(i).innerHTML = i;
+		}
+	}
+}
+
