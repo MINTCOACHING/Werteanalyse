e04b685643054794ac5f0ac142b0d61153188269
MINTCOACHING <74488802+MINTCOACHING@users.noreply.github.com>
2020-11-19 20:28:20 +0100

    Add files via upload

--- top10.js
+++ top10.js
@@ -1,5 +1,3 @@
-import { jsPDF } from 'jspdf.min.js';
-
 window.onload = clean;
 
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -103,10 +101,3 @@ function generatePDF() {
 	  
 	  
 
-function pdfdruck(){	  
-	var doc = new jsPDF()
-	doc.text(20, 20, 'Hello world!')
-	doc.text(20, 30, 'This is client-side Javascript, pumping out a PDF.')
-	doc.addPage()
-	doc.text(20, 20, 'Do you like that?')
-}
