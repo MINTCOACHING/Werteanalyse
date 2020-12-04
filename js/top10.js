528314025e774a9a7b6b178bd0ff3035bf90dd97
MINTCOACHING <74488802+MINTCOACHING@users.noreply.github.com>
2020-12-04 17:27:23 +0100

    v1.0

--- js/top10.js
+++ js/top10.js
@@ -93,7 +93,6 @@ function generatePDF() {
 		  image:        { type: 'jpeg', quality: 0.98 },
 		  html2canvas:  { scale: 4,},
       jsPDF:        { unit: 'in', format: 'letter', orientation: 'landscape' }
-      useCORS: true,
 };
 
 		// New Promise-based usage:
